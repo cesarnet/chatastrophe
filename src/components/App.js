14989c495faca4a515d4f3bb8438604359636de8
Cesar Sandin <cesar.sandin@gmail.com>
2018-02-12 16:51:07 +0000

    LoginContainer

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import LoginContainer from './LoginContainer';
+import './app.css';
+
+const App = () => {
+  return (
+    <div id="container" className="inner-container">
+      <LoginContainer />
+    </div>
+  );
+};
+
+export default App;
